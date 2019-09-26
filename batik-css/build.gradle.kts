86a7539cdbb8afa889ce04158dc3cc5ae546d46b
Nikolas Havrikov <havrikov@cs.uni-saarland.de>
2019-09-26 15:58:38 +0200

    batik-css now has dependency in the correct subject configuration
    (cherry picked from commit 8ff3a689255ec01d4b308d6df32ec64c39fc722a)

--- batik-css/build.gradle.kts
+++ batik-css/build.gradle.kts
@@ -1,4 +1,4 @@
 dependencies {
     implementation(project(":sac-driver"))
-    compile("org.apache.xmlgraphics","batik-css", Deps.batikCssVersion)
+    subject("org.apache.xmlgraphics","batik-css", Deps.batikCssVersion)
 }
