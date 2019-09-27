2daf64b5ad2a099d734c4f7f4979a679a0e5bc60
Nikolas Havrikov <havrikov@cs.uni-saarland.de>
2019-09-27 15:52:37 +0200

    Using proper artifact dependency for ph-css
    (cherry picked from commit 2af6a51ded2452b132a1450918eb28374b7e421b)

--- ph-css/build.gradle.kts
+++ ph-css/build.gradle.kts
@@ -1,4 +1,3 @@
 dependencies {
-    subject("com.helger.maven", "ph-csscompress-maven-plugin", Deps.phCssVersion)
-    //subject("com.helger", "ph-css-parent-pom", Deps.phCssVersion)
+    subject("com.helger", "ph-css", Deps.phCssVersion)
 }
