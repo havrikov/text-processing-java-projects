9ca1683d844a7a9dc20b51fa2d03796e1cb2ca69
Nikolas Havrikov <havrikov@cs.uni-saarland.de>
2019-11-27 16:20:49 +0100

    Separating jacoco concerns
    (cherry picked from commit 33ec5cfbfdbc1fd032c7b105643fe8192922c694)

---
+++ utils/src/main/kotlin/saarland/cispa/subjects/coverage/CoverageExtractor.kt
@@ -0,0 +1,40 @@
+package saarland.cispa.subjects.coverage
+
+import org.jacoco.agent.rt.internal_43f5073.Agent
+import org.jacoco.agent.rt.internal_43f5073.core.runtime.AgentOptions
+import org.jacoco.core.analysis.Analyzer
+import org.jacoco.core.analysis.CoverageBuilder
+import org.jacoco.core.analysis.IClassCoverage
+import org.jacoco.core.data.ExecutionDataReader
+import org.jacoco.core.data.ExecutionDataStore
+import java.io.File
+
+class CoverageExtractor(private val originalByteCode: File?, packagePrefix: String) {
+    private val agent = Agent.getInstance(AgentOptions("output=none,dumponexit=false,excludes=*,includes=$packagePrefix*"))
+    private val original: ByteArray
+
+    init {
+        require(originalByteCode != null && originalByteCode.exists()) { "The path to the original bytecode is invalid! Given: $originalByteCode" }
+        original = originalByteCode.readBytes()
+        // reset any coverage from startup if there is any
+        agent.reset()
+    }
+
+    internal fun getCoveredClasses(): Collection<IClassCoverage> {
+        val coverageBuilder = CoverageBuilder()
+        val executionData = getExecutionData()
+        val analyzer = Analyzer(executionData, coverageBuilder)
+        analyzer.analyzeAll(original.inputStream(), originalByteCode.toString())
+        return coverageBuilder.classes
+    }
+
+    private fun getExecutionData(): ExecutionDataStore {
+        val executionData = agent.getExecutionData(false)
+        val executionDataReader = ExecutionDataReader(executionData.inputStream())
+        val execStore = ExecutionDataStore()
+        executionDataReader.setExecutionDataVisitor(execStore)
+        executionDataReader.setSessionInfoVisitor {} // must avoid NPE
+        executionDataReader.read()
+        return execStore
+    }
+}
