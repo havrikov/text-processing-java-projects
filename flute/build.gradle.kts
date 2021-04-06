b27136a97134d7e39f152b68532a28ba56dab1da
Nikolas Havrikov <nikolas.havrikov@cispa.saarland>
2021-04-06 19:55:58 +0200

    Subject milyn.flute requires the jcenter repo :(
    (cherry picked from commit 06917a168af4f9a939a4504e054f6d3c985d9366)

--- flute/build.gradle.kts
+++ flute/build.gradle.kts
@@ -1,3 +1,12 @@
+// The version from mavenCentral() has the following error:
+// inconsistent module metadata found. Descriptor: milyn:flute:1.3 Errors: bad group: expected='org.milyn' found='milyn'
+// However, jcenter() is going to be discontinued:
+// https://jfrog.com/blog/into-the-sunset-bintray-jcenter-gocenter-and-chartcenter/
+// Therefore, who knows how long this will work.
+repositories {
+    jcenter()
+}
+
 dependencies {
     implementation(project(":sac-driver"))
     subject("org.milyn", "flute", Deps.fluteVersion)
