f95db3bfc112fb442209fa18dc59987b894760fb
Nikolas Havrikov <nikolas.havrikov@cispa.saarland>
2021-04-07 11:56:35 +0200

    Returned the missing externs.zip for the Google Closure driver
    (This partially reverts 88631333027a6e548cc37a9a6f474903862167b5)
    
    (cherry picked from commit 244d6198a330ffc1c21e6c41289d87ed85f03143)

--- closure/build.gradle.kts
+++ closure/build.gradle.kts
@@ -1,3 +1,4 @@
 dependencies {
     subject("com.google.javascript", "closure-compiler", Deps.closureVersion)
+    runtimeOnly("com.google.javascript", "closure-compiler-externs", Deps.closureVersion)
 }
